82002111427769e2c992f453cf3f50a21d4de6d4
Jerry <chih.chia.chuang@gmail.com>
2019-06-11 03:10:08 +0000

    refactor chatty_server/server.js to use switch instead of if

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,24 +1,17 @@
-// server.js
-
 const express = require('express');
 const WebSocket = require('ws');
 
-// Set the port to 3001
 const PORT = 3001;
 const uuidv1 = require('uuid/v1');
 
-// Create a new express server
+
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
-// Create the WebSockets server
+
 const wss = new WebSocket.Server({ server });
 
-// Set up a callback that will run when a client connects to the server
-// When a client connects they are assigned a socket, represented by
-// the ws parameter in the callback.
 function Broadcast(message) {
   wss.clients.forEach(function each(client){
     if (client.readyState === WebSocket.OPEN) {
@@ -41,16 +34,18 @@ wss.on('connection', (ws) => {
   ws.on('message', function incoming(data){
     console.log('received message')
     let received = JSON.parse(data)
-    if (received.type === 'postMessage'){
-      received.type = 'incomingMessage'
-      received.id = uuidv1();
-      Broadcast(JSON.stringify(received))
-    }
-    if (received.type === 'postNotification'){
-      received.type = 'incomingNotification'
-      received.id = uuidv1();
-      console.log('broadcasting notification', received)
-      Broadcast(JSON.stringify(received))
+    switch (received.type){
+      case 'postMessage':
+        received.type = 'incomingMessage'
+        received.id = uuidv1();
+        Broadcast(JSON.stringify(received))
+        break;
+      case 'postNotification':
+        received.type = 'incomingNotification'
+        received.id = uuidv1();
+        console.log('broadcasting notification', received)
+        Broadcast(JSON.stringify(received))
+        break;
     }
   })
 

807866da053cd50ca92e1f05e1840093157dabd9
Jerry <chih.chia.chuang@gmail.com>
2019-06-12 01:50:41 +0000

    Refactor Nav.jsx to be function component

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,13 +1,10 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Nav extends Component {
-  render() {
-    return (
-      <nav className="navbar">
-        <a href="/" className="navbar-brand">Chatty</a>
-        <span className ="navbar-count">{this.props.userCount} users online</span>
-      </nav>
-    );
-  }
-}
+const Nav = (props) =>(
+  <nav className="navbar">
+    <a href="/" className="navbar-brand">Chatty</a>
+    <span className ="navbar-count">{props.userCount} users online</span>
+  </nav>
+)
+
 export default Nav;
